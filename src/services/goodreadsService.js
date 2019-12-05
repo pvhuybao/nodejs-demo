13d398850a31c4784b0221b569622943a1f982db
pvhuybao2210 <pvhuybao2210@gmai.com>
2019-12-05 23:53:53 +0700

    commit code

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,8 +1,27 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
 
+const parser = xml2js.Parser({ explicitArray: false });
 
 function goodreadsService() {
-    function getBookById() {
+    function getBookById(id) {
         return new Promise((resolve, reject) => {
+            axios.get('https://url')
+                .then((response) => {
+                    parser.parseString(response.data, (err, result) => {
+                        if (err) {
+                            debug(err);
+                        } else {
+                            debug(result);
+                            resolve(result.GoodreadsResponse.book);
+                        }
+                    });
+                })
+                .catch((error) => {
+                    reject(error);
+                    debug(error);
+                });
             resolve({ description: 'our description' });
         });
     }
